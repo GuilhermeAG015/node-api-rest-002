7a6679df41dc2359bb03b2296765e61bf5b606e3
Guilherme Alexandre <guilhermegrilo2009@hotmail.com>
2025-04-01 19:51:19 -0300

    Adding new route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
+app.get('/hello-v2', async () => {
+  const tansactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return tansactions
+})
+
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const tansaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return tansaction
 })
 
 app
